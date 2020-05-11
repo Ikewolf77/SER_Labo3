6966033dc4bb880c032384c5abb9a35c2dc710f0
Simon <simon.mattei@heig-vd.ch>
2020-05-11 12:24:53 +0200

    there are unltipolygons thinking

--- src/JDOM2Manager.java
+++ src/JDOM2Manager.java
@@ -7,9 +7,25 @@
 import java.io.*;
 import org.jdom2.*;
 import org.jdom2.output.*;
+import org.json.simple.JSONObject;
 
 public class JDOM2Manager {
 
+    private String XMLPath;
+    private Document file;
 
+    public JDOM2Manager(String output){
+        XMLPath = output;
+    }
+
+    public void writeKMLPolygon(JSONObject polygon){
+        System.out.println(((JSONObject)polygon.get("geometry")).get("type"));
+    }
+
+    public void flush() throws IOException {
+        XMLOutputter xmlOutputter = new XMLOutputter();
+        xmlOutputter.setFormat(Format.getPrettyFormat());
+        xmlOutputter.output(file, new FileWriter(XMLPath));
+    }
 
 }
--- src/JSONManager.java
+++ src/JSONManager.java
@@ -4,8 +4,6 @@
  * 31.05.2020
  */
 
-
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -20,23 +18,38 @@ public class JSONManager {
     private JSONParser parser;
     private String file;
 
+    /**
+     * Constructor
+     * @param file path to parse
+     */
     public JSONManager(String file){
         parser = new JSONParser();
         this.file = file;
     }
 
+    /**
+     * Parsing geoJson file to KML
+     * @param output output file
+     */
     public void parseKML(String output) {
 
         try (FileReader reader = new FileReader(file)) {
 
-            // lecture du fichier
+            JDOM2Manager writer = new JDOM2Manager(output);
+
+            //reading file
             Object obj = parser.parse(reader);
 
-            JSONObject file = (JSONObject) obj;
-            System.out.println(file);
+            //parsing each country
+            JSONArray features = (JSONArray) ((JSONObject)obj).get("features");
+            for(Object country : features){
+                writer.writeKMLPolygon((JSONObject)country);
+            }
 
+            //flush on output file
+            writer.flush();
 
-        } catch (IOException | ParseException e) {
+        } catch (IOException | ParseException e ) {
             e.printStackTrace();
         }
 
