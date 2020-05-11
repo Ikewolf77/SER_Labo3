f0f078ce4e6c1e875b9ae4794e206e236a88f1ce
Simon <simon.mattei@heig-vd.ch>
2020-05-11 11:08:25 +0200

    added ressources

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,18 @@
  * 31.05.2020
  */
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.*;
+import org.jdom2.*;
+import org.jdom2.output.*;
 
 public class Main {
 
